22a5c5bbb72336225f01841854560321f1836158
Adam Alcorn <aalcorn@stu.jsu.edu>
2018-10-03 12:11:51 -0500

    Testing and update

--- Alpha Project/src/alpha/project/AlphaProject.java
+++ Alpha Project/src/alpha/project/AlphaProject.java
@@ -3,6 +3,8 @@ package alpha.project;
 public class AlphaProject {
 
     public static void main(String[] args) {
+        //SELECT id, terminalid, badgeid, punchtypeid, UNIX_TIMESTAMP(originaltimestamp) * 1000 AS originaltimestamp FROM punch p;
+        //Above is the query Snellen recommended for getting the timestamp in terms of milliseconds passed since 1970, which can then be converted into a gregorian calendar object. 
         //Testing
         //Amber Testing
         //Adam Testing
