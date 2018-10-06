877be1bc4b00ea791c2d8bdbfdb7cbb39dfe8d75
Amber Rampey <arampey@stu.jsu.edu>
2018-10-06 18:11:22 -0500

    Shift class WIP

--- Alpha Project/src/alpha/project/Shift.java
+++ Alpha Project/src/alpha/project/Shift.java
@@ -7,8 +7,48 @@ package alpha.project;
 
 /**
  *
- * @author Adam
+ * @author Amber
+ * description, shiftstart, shiftstop, interval, graceperiod, dock, lunchstart, lunchstop, lunchdeduct
  */
 public class Shift {
     
-}
+    public String getShiftDesc() {
+        return desc;
+    }
+    
+    public long getShiftStart() {
+        return shift_start;
+    }
+    
+    
+    public long getShiftStop() {
+        return shift_stop;
+    }
+    
+    public int getInterval() {
+        return interval;
+    }
+    
+    public int getGracePeriod() {
+        return grace_period;
+    }
+    
+    public int getDock() {
+        return dock;
+    }
+    
+    public long getLunchStart () {
+        return lunch_start;
+    }
+    
+    public long getLunchStop () {
+        return lunch_stop;
+    }
+    
+    public int getLunchDeduct () {
+        return lunch_deduct;
+    }
+    
+    
+    }
+
