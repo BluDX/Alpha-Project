605710a718ce8c22f10805ec8e92a32c6430d0d1
Amber Rampey <arampey@stu.jsu.edu>
2018-10-08 12:29:27 -0500

    Shift.java updates

--- Alpha Project/src/alpha/project/Shift.java
+++ Alpha Project/src/alpha/project/Shift.java
@@ -8,47 +8,115 @@ package alpha.project;
 /**
  *
  * @author Amber
+ * TODO: toString, gregorian calendar covnersion
  * description, shiftstart, shiftstop, interval, graceperiod, dock, lunchstart, lunchstop, lunchdeduct
- */
+ */  
+
 public class Shift {
     
+    String shiftDesc;
+    long shiftStart;
+    long shiftStop;
+    int interval;
+    int gracePeriod;
+    int dock;
+    long lunchStart;
+    long lunchStop;
+    int lunchDeduct;
+    
+    // Getters and setters
     public String getShiftDesc() {
-        return desc;
+        return shiftDesc;
     }
-    
+
+    public void setShiftDesc(String shiftDesc) {
+        this.shiftDesc = shiftDesc;
+    }
+
+    // shiftStart
     public long getShiftStart() {
-        return shift_start;
+        return shiftStart;
     }
-    
-    
+
+    public void setShiftStart(long shiftStart) {
+        this.shiftStart = shiftStart;
+    }
+
+    // shiftStop
     public long getShiftStop() {
-        return shift_stop;
+        return shiftStop;
     }
-    
+
+    public void setShiftStop(long shiftStop) {
+        this.shiftStop = shiftStop;
+    }
+
+    // interval
     public int getInterval() {
         return interval;
     }
-    
-    public int getGracePeriod() {
-        return grace_period;
+
+    public void setInterval(int interval) {
+        this.interval = interval;
     }
-    
+
+    //gracePeriod
+    public int getGracePeriod() {
+        return gracePeriod;
+    }
+
+    public void setGracePeriod(int gracePeriod) {
+        this.gracePeriod = gracePeriod;
+    }
+
+    // dock
     public int getDock() {
         return dock;
     }
-    
-    public long getLunchStart () {
-        return lunch_start;
-    }
-    
-    public long getLunchStop () {
-        return lunch_stop;
-    }
-    
-    public int getLunchDeduct () {
-        return lunch_deduct;
-    }
-    
-    
+
+    public void setDock(int dock) {
+        this.dock = dock;
     }
 
+    //lunchStart
+    public long getLunchStart() {
+        return lunchStart;
+    }
+
+    public void setLunchStart(long lunchStart) {
+        this.lunchStart = lunchStart;
+    }
+
+    // lunchStop
+    public long getLunchStop() {
+        return lunchStop;
+    }
+
+    public void setLunchStop(long lunchStop) {
+        this.lunchStop = lunchStop;
+    }
+
+    // lunchDeduct
+    public int getLunchDeduct() {
+        return lunchDeduct;
+    }
+
+    public void setLunchDeduct(int lunchDeduct) {
+        this.lunchDeduct = lunchDeduct;
+    }
+
+    public Shift(String shiftDesc, long shiftStart, long shiftStop, int interval, int gracePeriod, int dock, long lunchStart, long lunchStop, int lunchDeduct) {
+        this.shiftDesc = shiftDesc;
+        this.shiftStart = shiftStart;
+        this.shiftStop = shiftStop;
+        this.interval = interval;
+        this.gracePeriod = gracePeriod;
+        this.dock = dock;
+        this.lunchStart = lunchStart;
+        this.lunchStop = lunchStop;
+        this.lunchDeduct = lunchDeduct;
+    }
+
+    
+    
+}
