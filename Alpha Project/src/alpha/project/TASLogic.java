61330ebf2ed5eafe356c722e47769d85683efea2
Hunter Mays <hmays@stu.jsu.edu>
2018-11-07 22:18:59 -0600

    Updated TASLogic. I am having a problem with the data type conversions

--- Alpha Project/src/alpha/project/TASLogic.java
+++ Alpha Project/src/alpha/project/TASLogic.java
@@ -5,6 +5,11 @@
  */
 package alpha.project;
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
 
 /**
  *
@@ -13,7 +18,47 @@ import java.util.ArrayList;
  */
 public class TASLogic {
     public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
-        int totalMinutes = 0;
+       
+         //Declare variables
+        
+        int totalMinutes;
+        long shiftStart;
+        long shiftEnd;
+        long lunchStart;
+        long lunchEnd;
+        
+        /*Get times from dailypunchlist and check to see if they took a lunch*/
+	
+	//This is if they clocked out for a lunch
+        
+        if(dailypunchlist.size() == 4){ 
+            
+            shiftStart = dailypunchlist.get(0).getOriginaltimestamp();
+            shiftEnd = dailypunchlist.get(3).getOriginaltimestamp();
+            lunchStart = dailypunchlist.get(1).getOriginaltimestamp();
+            lunchEnd = dailypunchlist.get(2).getOriginaltimestamp();
+            
+            totalMinutes = (shiftEnd - shiftStart - 1800000) / 60000;
+        }
+        
+        //This is if they did not clock out for a lunch
+        
+        else if(dailypunchlist.size() == 2){ 
+            
+            shiftStart = dailypunchlist.get(0).getOriginaltimestamp();
+            shiftEnd = dailypunchlist.get(1).getOriginaltimestamp();
+            
+            totalMinutes = (shiftEnd - shiftStart) / 60000;
+            
+            //This checks to see if they worked the correct amount of time to get a lunch
+            
+            if(totalMinutes >= 360) 
+               
+                totalMinutes = totalMinutes - 1800000;
+        }
+        
+        
+        
         
         
         
